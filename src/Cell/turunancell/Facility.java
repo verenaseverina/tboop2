6566d96e35b408fbc33ba351e40ceaac7ee7184c
verenaseverina <verenaseverina@gmail.com>
2017-03-26 13:02:37 +0700

    nambahin Facility.java

---
+++ src/Cell/turunancell/Facility.java
@@ -0,0 +1,28 @@
+package Cell.turunancell;
+
+/**
+ * Created by verenaseverina on 3/26/17.
+ */
+import Cell.Cell;
+public class Facility extends Cell {
+  private boolean entrance = false;
+  private boolean exit = false;
+
+  public Facility(char _content, int i, int j) {
+    super(_content, i, j);
+  }
+  public boolean IsEntrance() {
+    return entrance;
+  }
+  public boolean IsExit() {
+    return exit;
+  }
+  public void SetTrue(int n) {
+    if (n == 0) {
+      entrance = true;
+    }
+    else if (n == 1) {
+      exit = true;
+    }
+  }
+}
