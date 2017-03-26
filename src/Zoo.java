c0b8b09ed7f7e72383d110d1774035fdeac49b65
verenaseverina <verenaseverina@gmail.com>
2017-03-26 12:10:35 +0700

    nambahin Zoo.java tapi belom ada isinya

---
+++ src/Zoo.java
@@ -0,0 +1,70 @@
+import java.util.Vector;
+import Cell.Cell;
+import Cell.turunancell.Habitat;
+import Animal.Animal;
+
+public class Zoo {
+  private Vector<Cage> cages;
+  private Cell playerpos;
+  private Cell[][] map;
+  private int height;
+  private int width;
+
+  public Zoo() {
+
+  }
+  public void GetAllCage(State s) {
+
+  }
+  public Vector<Habitat> GetCage(Cell h) {
+
+  }
+  public boolean InCage(Vector<Habitat> v, Cell c) {
+
+  }
+  public Cell[][] GetMap() {
+
+  }
+  public Vector<Cage> GetCages() {
+
+  }
+  public int GetHeight() {
+
+  }
+  public int GetWidth() {
+
+  }
+  public boolean IsHabitat(char c) {
+
+  }
+  public boolean IsFacility(char c) {
+
+  }
+  public void PutInAnimal() {
+
+  }
+  public void CheckCage(boolean[] arr, Animal[] animal) {
+
+  }
+  public void Tour() {
+
+  }
+  public void TourInteract(Cell pos) {
+
+  }
+  public void RandomEntrance() {
+
+  }
+  public boolean Exit(Cell pos) {
+
+  }
+  public boolean IsPlayer(int i, int j) {
+
+  }
+  public boolean IsHabitat(char c) {
+
+  }
+  public boolean IsRoad(Cell c) {
+    
+  }
+}
