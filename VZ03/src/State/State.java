e99be5e8f946ef74028bc66dd631969a56c13d4b
adrianhp97 <adrianhp100797@gmail.com>
2017-03-28 09:23:17 +0700

    bisa default

--- VZ03/src/State/State.java
+++ VZ03/src/State/State.java
@@ -1,9 +1,7 @@
 package State;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class State {
@@ -12,65 +10,63 @@ public class State {
   private int height;
   
   public State() {
-    try {
-      BufferedReader buffer_map;
-      buffer_map = new BufferedReader(new FileReader("VZ03/src/map.txt"));
-      try {
-        StringBuilder string_builder_map = new StringBuilder();
-        String line = buffer_map.readLine();
-        
-        while (line != null) {
-          string_builder_map.append(line);
-          line = buffer_map.readLine();
-        }
-        String string_buffer_map;
-        string_buffer_map = string_builder_map.toString();
-        //System.out.printf(string_buffer_map);
-        
-        int i = 0;
-        int s_height;
-        height=0;
-        while(string_buffer_map.charAt(i) != ' ') {
-          s_height=Character.getNumericValue(string_buffer_map.charAt(i));
-          height = (10 * height) + s_height;
-          i++;
-        }
-        i=i+3;
-        
-        int s_width;
-        width=0;
-        while(string_buffer_map.charAt(i) <= '9' && string_buffer_map.charAt(i) >= '0') {
-          s_width=Character.getNumericValue(string_buffer_map.charAt(i));
-          width = (10 * width) + s_width;
-          i++;
-        }
-        
-        int k = 0, l = 0;
-        map = new char [height][width];
-        //char[] temp_string_char = string_buffer_map.toCharArray();
-        int length = string_buffer_map.length();
-        while (i < length) {
-          if (l < width) {
-            map[k][l] = string_buffer_map.charAt(i);
-            l++;
-            i++;
-          }
-          else {
-            k++;
-            l=0;
-          }
-        }
-        buffer_map.close();
-      } catch(Exception E) {
-        MakeDefaultMap();
+    //BufferedReader buffer_map;
+    // buffer_map = new BufferedReader(new FileReader("src/map.txt"));
+    try (BufferedReader buffer_map = new BufferedReader(new FileReader("src/map.txt"))) {
+      StringBuilder string_builder_map = new StringBuilder();
+      String line = buffer_map.readLine();
+
+      while (line != null) {
+        string_builder_map.append(line);
+        line = buffer_map.readLine();
       }
-    } catch(FileNotFoundException ex) {
-      Logger.getLogger(State.class.getName()).log(Level.SEVERE, null, ex);  
+      String string_buffer_map;
+      string_buffer_map = string_builder_map.toString();
+      //System.out.printf(string_buffer_map);
+
+      int i = 0;
+      int s_height;
+      height=0;
+      while(string_buffer_map.charAt(i) != ' ') {
+        s_height=Character.getNumericValue(string_buffer_map.charAt(i));
+        height = (10 * height) + s_height;
+        i++;
+      }
+      i=i+3;
+
+      int s_width;
+      width=0;
+      while(string_buffer_map.charAt(i) <= '9' && string_buffer_map.charAt(i) >= '0') {
+        s_width=Character.getNumericValue(string_buffer_map.charAt(i));
+        width = (10 * width) + s_width;
+        i++;
+      }
+
+      int k = 0, l = 0;
+      map = new char [height][width];
+      //char[] temp_string_char = string_buffer_map.toCharArray();
+      int length = string_buffer_map.length();
+      while (i < length) {
+        if (l < width) {
+          map[k][l] = string_buffer_map.charAt(i);
+          l++;
+          i++;
+        }
+        else {
+          k++;
+          l=0;
+        }
+      }
+      buffer_map.close();
+    } catch(Exception E) {
+      MakeDefaultMap();
     }
   }
   
   private void MakeDefaultMap() {
     map = new char [8][8];
+    width = 8;
+    height = 8;    
     
     for (int i = 0; i < 8 ; i++) {
       for (int j = 0; j < 8; j++) {
@@ -88,6 +84,7 @@ public class State {
             else if (j < 8) {
               map[i][j] = '#';
             }
+            break;
           }
           case 1 : {
             if (j < 2) {
@@ -99,6 +96,7 @@ public class State {
             else if (j < 8) {
               map[i][j] = '~';
             }
+            break;
           }
           case 2 : {
             if (j < 3) {
@@ -113,6 +111,7 @@ public class State {
             else if (j < 8) {
               map[i][j] = '~';
             }
+            break;
           }
           case 3 : {
             if (j < 1) {
@@ -127,6 +126,7 @@ public class State {
             else if (j < 8) {
               map[i][j] = '_';
             }
+            break;
           }
           case 4 : {
             if (j < 1) {
@@ -144,6 +144,7 @@ public class State {
             else if (j < 8) {
               map[i][j] = '#';
             }
+            break;
           }
           case 5 : {
             if (j < 1) {
@@ -155,6 +156,7 @@ public class State {
             else if (j < 8) {
               map[i][j] = '#';
             }
+            break;
           }
           case 6 : {
             if (j < 5) {
@@ -163,14 +165,15 @@ public class State {
             else if (i < 8) {
               map[i][j] = '#';
             }
+            break;
           }
           case 7 : {
             map[i][j] = '_';
+            break;
           }
         }
       }
     }
-    
   }
   
   public char[][] GetMap() {
