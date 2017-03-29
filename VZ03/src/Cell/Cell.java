3a7a8e10dcc4386aa6613d4e899800787085e950
kukuhbr <kukuhis777@gmail.com>
2017-03-29 08:56:02 +0700

    javadocs facil

--- VZ03/src/Cell/Cell.java
+++ VZ03/src/Cell/Cell.java
@@ -1,34 +1,94 @@
+/**
+ * Created by Kebun Binatang Bandung on 3/26/17.
+ */
 package cell;
 
+/**
+ * Kelas sebuah block 1 meter kali 1 meter didalam Zoo.
+ * <p>
+ *     Suatu Cell dapat berupa Facility atau Habitat sebagai kelas
+ *     turunannya. Untuk properti perbedaan dari Facility dan Habitat
+ *     dapat dilihat pada dokumentasi kelas yang bersangkutan.
+ * </p>
+ * <p>
+ *     Cell memiliki tiga atribut yaitu isi dari Cell tersebut berupa karakter
+ *     dan indeks baris dan kolom yang keduanya berupa integer.
+ * </p>
+ * @see cell.turunancell.Facility
+ * @see cell.turunancell.Habitat
+ *
+ * @author Kukuh Basuki Rahmat
+ * @author Verena Severina
+ */
 public class Cell {
+  /**
+   * Atribut konten karakter dari Cell
+   */
   protected char content;
+  /**
+   * Atribut indeks baris dari Cell
+   */
   protected int cellIdRow;
+  /**
+   * Atribut indeks kolom dari Cell
+   */
   protected int cellIdCol;
 
+  /**
+   * Konstruktor Cell Default.
+   * Memiliki nilai-nilai atribut default:
+   * <ul>
+   *     <li>content = \0</li>
+   *     <li>cellIdRow = -1</li>
+   *     <li>cellIdCol = -1</li>
+   * </ul>
+   */
   public Cell() {
     content = '\0';
     cellIdRow = -1;
     cellIdCol = -1;
   }
 
+  /**
+   * Konstruktor Cell dengan parameter.
+   * @param contentInput nilai konten Cell
+   * @param xrow baris dari Cell
+   * @param ycol kolom dari Cell
+   */
   public Cell(char contentInput, int xrow, int ycol) {
     content = contentInput;
     cellIdRow = xrow;
     cellIdCol = ycol;
   }
 
+  /**
+   * Getter content Cell.
+   * @return karakter konten Cell
+   */
   public char getCellContent() {
     return content;
   }
 
+  /**
+   * Getter cellIdRow Cell.
+   * @return integer baris lokasi Cell
+   */
   public int getCellRow() {
     return cellIdRow;
   }
 
+  /**
+   * Getter cellIdCol Cell.
+   * @return integer kolom lokasi Cell
+   */
   public int getCellCol() {
     return cellIdCol;
   }
 
+  /**
+   *
+   * @param nx
+   */
   public void setTrueEntranceExit(int nx) {
     nx = 0;
   }
--- VZ03/src/Cell/turunancell/Facility.java
+++ VZ03/src/Cell/turunancell/Facility.java
@@ -1,23 +1,58 @@
+/**
+ * Created by Kebun Binatang Bandung on 3/26/17.
+ */
 package cell.turunancell;
 
 import cell.Cell;
 
+/**
+ * Kelas spesifikasi Cell sebagai Facility untuk pengunjung.
+ * <p>
+ *     Kelas Facility memiliki atribut berupa boolean atau nilai
+ *     kebenaran exit dan entrance.
+ * </p>
+ */
 public class Facility extends Cell {
+  /**
+   * Atribut entrance bernilai true apabila Facility adalah pintu masuk Zoo.
+   */
   private boolean entrance = false;
+  /**
+   * Atribut exit bernilai true apabila Facility adalah pintu keluar Zoo.
+   */
   private boolean exit = false;
 
+  /**
+   * Konstruktor Facility.
+   * @param contentInput simbol dari Facility
+   * @param iidx urutan baris dari Facility
+   * @param jidx urutan kolom dari Facility
+   */
   public Facility(char contentInput, int iidx, int jidx) {
     super(contentInput, iidx, jidx);
   }
 
+  /**
+   * Getter atribut entrance.
+   * @return boolean eantrace
+   */
   public boolean isEntrance() {
     return entrance;
   }
 
+  /**
+   * Getter atribut exit.
+   * @return boolean exit
+   */
   public boolean isExit() {
     return exit;
   }
 
+  /**
+   * Memberikan nilai true untuk entrance atau exit pada suatu Facility.
+   * @param n integer command untuk memilih atribut yang di set true,
+   *          1 untuk entrance dan 2 untuk exit.
+   */
   public void setTrue(int n) {
     if (n == 0) {
       entrance = true;
