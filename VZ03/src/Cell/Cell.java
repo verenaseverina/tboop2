02b14a4810a17c4ca7581ae6f582030f65bd4b11
verenaseverina <verenaseverina@gmail.com>
2017-03-29 07:02:46 +0700

    Cell dah sesuai checkstyle tapi belom ditambahin JavaDocs

--- VZ03/src/Cell/Cell.java
+++ VZ03/src/Cell/Cell.java
@@ -1,8 +1,5 @@
 package cell;
 
-/**
- * Created by verenaseverina on 3/25/17.
- */
 public class Cell {
   protected char content;
   protected int cellIdRow;
@@ -13,26 +10,33 @@ public class Cell {
     cellIdRow = -1;
     cellIdCol = -1;
   }
-  public Cell(char contentInput, int xRow, int yCol) {
+
+  public Cell(char contentInput, int xrow, int ycol) {
     content = contentInput;
-    cellIdRow = xRow;
-    cellIdCol = yCol;
+    cellIdRow = xrow;
+    cellIdCol = ycol;
   }
+
   public char getCellContent() {
     return content;
   }
+
   public int getCellRow() {
     return cellIdRow;
   }
+
   public int getCellCol() {
     return cellIdCol;
   }
-  public void SetTrueEntranceExit(int nX) {
-    nX = 0;
+
+  public void setTrueEntranceExit(int nx) {
+    nx = 0;
   }
+
   public boolean isEntrance() {
     return false;
   }
+
   public boolean isExit() {
     return false;
   }
