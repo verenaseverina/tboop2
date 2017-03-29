cc77cfefc5cb3d02041ee9c419dd0592c76a11a9
verenaseverina <verenaseverina@gmail.com>
2017-03-29 12:26:23 +0700

    Cell VZ02

--- VZ02/src/Cell.java
+++ VZ02/src/Cell.java
@@ -1,34 +1,74 @@
 public class Cell {
+  /**
+   * Isi karakter dari Cell.
+   */
   private char content;
+  /**
+   * Baris dari Cell.
+   */
   private int cellIdRow;
+  /**
+   * Kolom dari Cell.
+   */
   private int cellIdCol;
+  /**
+   * boolean yang menyatakan apakah Cell merupakan entrance.
+   */
   private boolean entrance;
+  /**
+   * boolean yang menyatakan apakah Cell merupakan exit.
+   */
   private boolean exit;
 
+  /**
+   * Contructor.
+   */
   public Cell() {
     content = '\0';
     cellIdRow = -1;
     cellIdCol = -1;
   }
 
-  public Cell(char _content, int x, int y) {
-    content = _content;
-    cellIdRow = x;
-    cellIdCol = y;
+  /**
+   * Constructor.
+   * @param contentIn karakter yang akan diassign ke Cell
+   * @param xinput absis yang akan diassign ke Cell
+   * @param yinput ordinat yang akan diassign ke Cell
+   */
+  public Cell(char contentIn, int xinput, int yinput) {
+    content = contentIn;
+    cellIdRow = xinput;
+    cellIdCol = yinput;
   }
 
+  /**
+   * Getter Content Cell.
+   * @return Content dari Cell
+   */
   public char GetCellContent() {
     return content;
   }
 
+  /**
+   * Getter Baris dari Cell.
+   * @return Baris dari Cell
+   */
   public int GetCellRow() {
     return cellIdRow;
   }
 
+  /**
+   * Getter Kolom dari Cell.
+   * @return Kolom dari Cell
+   */
   public int GetCellCol() {
     return cellIdCol;
   }
 
+  /**
+   * Mengassign Cell merupakan entrance atau exit
+   * @param n boolean yang menyatakan apakah Cell entrance atau exit
+   */
   public void SetTrueEntranceExit(int n) {
     if (n == 0) {
       entrance = true;
