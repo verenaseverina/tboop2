f6d21de28ab2bb03e13043269692678c49b31d3a
verenaseverina <verenaseverina@gmail.com>
2017-03-29 07:27:23 +0700

    Renderable udah sesuai sama CheckStyle

--- VZ03/src/Renderable.java
+++ VZ03/src/Renderable.java
@@ -13,25 +13,25 @@ public class Renderable {
    * @param virtualZoo Zoo yang akan ditampilkan di layar console.
    */
   public void render(Zoo virtualZoo) {
-    int iIdx, jIdx;
-    int cageIdx, animalIdx;
-    for (iIdx = 0; iIdx < virtualZoo.GetHeight(); iIdx++) {
-      for (jIdx = 0; jIdx < virtualZoo.GetWidth(); jIdx++) {
-        if ((searchCageIndex(virtualZoo, iIdx, jIdx)) != -999) {
-          cageIdx = searchCageIndex(virtualZoo, iIdx, jIdx);
-          animalIdx = searchAnimalIndex(virtualZoo, cageIdx, iIdx, jIdx);
-          System.out.printf("%c", virtualZoo.GetCages().get(cageIdx).GetAnimal()[animal_idx].GetContent());
-        }
-        else if (virtualZoo.IsPlayer(iIdx, jIdx)) {
+    int iidx;
+    int jidx;
+    int cgIdx;
+    int anIdx;
+    for (iidx = 0; iidx < virtualZoo.getHeight(); iidx++) {
+      for (jidx = 0; jidx < virtualZoo.getWidth(); jidx++) {
+        if ((searchCageIndex(virtualZoo, iidx, jidx)) != -999) {
+          cgIdx = searchCageIndex(virtualZoo, iidx, jidx);
+          anIdx = searchAnimalIndex(virtualZoo, cgIdx, iidx, jidx);
+          System.out.printf("%c",virtualZoo.getCages().get(cgIdx).getAnimal()[anIdx].getContent());
+        } else if (virtualZoo.isPlayer(iidx, jidx)) {
           System.out.printf("P");
-        }
-        else {
-          System.out.printf("%c", virtualZoo.GetMap()[iIdx][jIdx].GetCellContent());
+        } else {
+          System.out.printf("%c", virtualZoo.getMap()[iidx][jidx].getCellContent());
         }
       }
       System.out.printf("\n");
     }
-      System.out.printf("\n");
+    System.out.printf("\n");
   }
 
   /**
@@ -39,31 +39,30 @@ public class Renderable {
    * ditemukan.
    *
    * @param virtualZoo Zoo yang ditinjau
-   * @param xRow Baris posisi Animal
-   * @param yCol Kolom posisi Animal
+   * @param xrow Baris posisi Animal
+   * @param ycol Kolom posisi Animal
    * @return integer indeks detemukannya animal
    */
-  public int searchCageIndex(Zoo virtualZoo, int xRow, int yCol) {
-    int iIdx = 0;
-    int jIdx = 0;
-    boolean found = false, stop;
+  public int searchCageIndex(Zoo virtualZoo, int xrow, int ycol) {
+    int iidx = 0;
+    int jidx = 0;
+    boolean found = false;
+    boolean stop;
     int cage = -999;
-    while ((iIdx < virtualZoo.GetCages().size()) && (!found)) {
+    while ((iidx < virtualZoo.getCages().size()) && (!found)) {
       stop = false;
-      while ((jIdx <=  virtualZoo.GetCages().get(iIdx).GetNeff()) && (!found) && (!stop)) {
-        if (virtualZoo.GetCages().get(iIdx).IsEmpty()) {
+      while ((jidx <=  virtualZoo.getCages().get(iidx).getNeff()) && (!found) && (!stop)) {
+        if (virtualZoo.getCages().get(iidx).isEmpty()) {
           stop = true;
-        }
-        else if ((virtualZoo.GetCages().get(iIdx).GetAnimal()[jIdx].GetX() == xRow)
-                && (virtualZoo.GetCages().get(iIdx).GetAnimal()[jIdx].GetY() == yCol)) {
+        } else if ((virtualZoo.getCages().get(iidx).getAnimal()[jidx].getX() == xrow)
+                && (virtualZoo.getCages().get(iidx).getAnimal()[jidx].getY() == ycol)) {
           found = true;
-          cage = iIdx;
-        }
-        else {
-          jIdx++;
+          cage = iidx;
+        } else {
+          jidx++;
         }
       }
-      iIdx++;
+      iidx++;
     }
     return cage;
   }
@@ -73,23 +72,22 @@ public class Renderable {
    * pada posisi baris x dan kolom y.
    *
    * @param virtualZoo Zoo yang ditinjau
-   * @param iIdx Indeks cage yang ditinjau
-   * @param xRow Baris posisi Animal
-   * @param yCol Kolom posisi Animal
+   * @param iidx Indeks cage yang ditinjau
+   * @param xrow Baris posisi Animal
+   * @param ycol Kolom posisi Animal
    * @return integer indeks animal pada posisi x,y dan cage indeks i
    */
-  public int searchAnimalIndex(Zoo virtualZoo, int iIdx, int xRow, int yCol) {
-    int kIdx = 0;
+  public int searchAnimalIndex(Zoo virtualZoo, int iidx, int xrow, int ycol) {
+    int kidx = 0;
     boolean found = false;
-    while ((kIdx < virtualZoo.GetCages().get(iIdx).GetCageSize()) && (!found)) {
-      if ((virtualZoo.GetCages().get(iIdx).GetAnimal()[k].GetX() == xRow)
-              && (virtualZoo.GetCages().get(iIdx).GetAnimal()[kIdx].GetY() == yCol)) {
+    while ((kidx < virtualZoo.getCages().get(iidx).getCageSize()) && (!found)) {
+      if ((virtualZoo.getCages().get(iidx).getAnimal()[kidx].getX() == xrow)
+              && (virtualZoo.getCages().get(iidx).getAnimal()[kidx].getY() == ycol)) {
         found = true;
-      }
-      else {
-        kIdx++;
+      } else {
+        kidx++;
       }
     }
-    return kIdx;
+    return kidx;
   }
 }
