25afae81519d345aa6b145673f1db0b83f497714
verenaseverina <verenaseverina@gmail.com>
2017-03-29 07:18:27 +0700

    Main udah sesuai sama CheckStyle

--- VZ03/src/Main.java
+++ VZ03/src/Main.java
@@ -1,10 +1,11 @@
 /**
  * Created by Kebun Binatang Bandung on 3/26/17.
  */
+import static java.lang.Thread.sleep;
+
 import java.io.IOException;
 import java.util.Scanner;
 
-import static java.lang.Thread.sleep;
 
 /**
  * Driver dari perangkat lunak virtual zoo.
@@ -12,57 +13,60 @@ import static java.lang.Thread.sleep;
  * @author Winarto
  */
 public class Main {
-    /**
-     * Driver dari perangkat lunak virtual zoo.
-     * Program mengkonstruksi sebuah objek Zoo dan ditampilkan ke layar.
-     * menggunakan kelas Renderable. Lalu program menerima user input
-     * untuk instruksi berikutnya yang terdiri dari:
-     * <ol>
-     *     <li>Tambahkan Animal.</li>
-     *     <li>Tour.</li>
-     *     <li>Exit.</li>
-     * </ol>
-     *
-     * @see Renderable
-     * @see Zoo
-     * @throws IOException Error handling
-     * @throws InterruptedException Error handling
-     */
-    public static void main() throws IOException,InterruptedException {
-      Zoo z = new Zoo();
-      int input;
-      Renderable r = new Renderable();
-      Scanner T = new Scanner(System.in);
-      boolean endloop = false;
-      z.RandomEntrance();
-      do {
-          System.out.print("1.Tambahkan Animal\n2. Tour\n3. Exit\n");
-          input = T.nextInt();
-          switch (input) {
-              case 1: {
-                  r.Render(z);
-                  z.PutInAnimal();
-                  break;
-              }
-              case 2: {
-                  z.RandomEntrance();
-                  r.Render(z);
-                  while(!z.IsExit(z.GetPlayerPos())) {
-                      sleep(500);
-                      z.Tour();
-                      for(int i= 0;i<z.GetCages().size();i++) {
-                          z.GetCages().get(i).UpdatePosition();
-                      }
-                      r.Render(z);
-                  }
-                  break;
-              }
-              case 3: {
-                  endloop = true;
-                  break;
-              }
+  /**
+   * Driver dari perangkat lunak virtual zoo.
+   * Program mengkonstruksi sebuah objek Zoo dan ditampilkan ke layar.
+   * menggunakan kelas Renderable. Lalu program menerima user input
+   * untuk instruksi berikutnya yang terdiri dari:
+   * <ol>
+   *     <li>Tambahkan Animal.</li>
+   *     <li>Tour.</li>
+   *     <li>Exit.</li>
+   * </ol>
+   *
+   * @see Renderable
+   * @see Zoo
+   * @throws IOException Error handling
+   * @throws InterruptedException Error handling
+   */
+  public static void main() throws IOException,InterruptedException {
+    Zoo z = new Zoo();
+    int input;
+    Renderable r = new Renderable();
+    Scanner tin = new Scanner(System.in);
+    boolean endloop = false;
+    z.randomEntrance();
+    do {
+      System.out.print("1.Tambahkan Animal\n2. Tour\n3. Exit\n");
+      input = tin.nextInt();
+      switch (input) {
+        case 1: {
+          r.render(z);
+          z.putInAnimal();
+          break;
+        }
+        case 2: {
+          z.randomEntrance();
+          r.render(z);
+          while (!z.isExit(z.getPlayerPos())) {
+            sleep(500);
+            z.tour();
+            for (int i = 0; i < z.getCages().size(); i++) {
+              z.getCages().get(i).updatePosition();
+            }
+            r.render(z);
           }
-      } while(!endloop);
-      
+          break;
+        }
+        case 3: {
+          endloop = true;
+          break;
+        }
+        default : {
+          break;
+        }
+      }
+    } while (!endloop);
+
   }
 }
